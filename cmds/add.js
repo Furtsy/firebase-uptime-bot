0d6ec5eaf38063b29b4ed101a6f114e4cac81876
Furtsy <43937364+Furtsy@users.noreply.github.com>
2020-11-11 11:33:18 +0300

    Create add.js

---
+++ cmds/add.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const urls = require('is-url');
+require('dotenv/config');
+const firebase = require('firebase/app');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+const admin = require('firebase-admin');
+const service = require('./service.json'); //firebase service json
+    admin.initializeApp({
+        credential: admin.credential.cert(service)
+      });
+exports.run = function(client, message, args) {
+let data = admin.firestore();
+  var link = args[1]
+  let url = urls(link) ? link : `https://${link}`;
+if(!urls(link)) return message.reply('please enter link')
+        const embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Successfully saved to the cloud')
+        .addField('Submitted by:', `<@${message.author.id}>`, true)
+        .addField('Added Link:', `${link}`, true)
+        .addField('Document Id:', `uptime.${message.author.id}`, true)
+        .setTimestamp();
+        message.channel.send(embed);
+data.collection('links').doc(`uptime.${message.author.id}`).set({
+'link': link,
+'author_id': message.author.id,
+'author_username': message.author.username
+})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["add"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'add',
+  description: 'Adds the link to be uptime to firebase',
+  usage: 'add'
+};
