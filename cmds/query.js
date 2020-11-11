6ec14c4d06f511dbc30fbc4c3b6263513b79336e
Furtsy <43937364+Furtsy@users.noreply.github.com>
2020-11-11 11:33:43 +0300

    Create query.js

---
+++ cmds/query.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const urls = require('is-url');
+require('dotenv/config');
+const firebase = require('firebase/app');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+const admin = require('firebase-admin');
+const service = require('./service.json'); //firebase service json
+    admin.initializeApp({
+        credential: admin.credential.cert(service)
+      });
+exports.run = function(client, message, args) {
+let data = admin.firestore();
+var link = args[1]
+var users = message.mentions.users.first()
+data.collection('links').doc(`uptime.${users.id}`).get().then((datas) => {
+if(!datas.exists) return message.channel.send('no links registered in data')
+        const embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .addField('Questioned Person:', `${datas.data().author_username}`, true)
+        .addField('Link:', `${datas.data().link}`, true)
+        .addField('Document Id:', `uptime.${datas.data().author_id}`, true)
+        .setTimestamp();
+        message.channel.send(embed);
+})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["query"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'query',
+  description: 'queries from the persons data',
+  usage: 'query'
+};
