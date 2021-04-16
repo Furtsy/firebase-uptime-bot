ad9d8edc9d222ebd3b3d0b9c4e88e435cc20465a
Furtsy <43937364+Furtsy@users.noreply.github.com>
2021-04-16 14:07:53 +0300

    Update add.js

--- cmds/add.js
+++ cmds/add.js
@@ -7,6 +7,9 @@ const admin = require('firebase-admin');
 exports.run = function(client, message, args) {
 let data = admin.firestore();
   var link = args[1]
+let url = urls(link) ? link : `https://${link}`;
+if(!urls(url)) return message.reply('please enter link')
+
 if(!urls(url)) return message.reply('please enter link')
         const embed = new Discord.MessageEmbed()
         .setColor('RANDOM')
